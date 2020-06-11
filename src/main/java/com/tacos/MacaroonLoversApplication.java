c04fe5f108241a0e46509d81c12464b455b543d7
thisisspear <rjsckdd12@gmail.com>
2020-06-11 22:37:42 +0900

    initial commit

---
+++ src/main/java/com/tacos/MacaroonLoversApplication.java
@@ -0,0 +1,13 @@
+package com.tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacaroonLoversApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacaroonLoversApplication.class, args);
+	}
+
+}
